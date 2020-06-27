a0d58777b03676d82d24a9d1c233751fee1f97c6
Christina Tarango <liwentarango@gmail.com>
2020-06-26 19:27:42 -0700

    Updates

---
+++ precache-manifest.56d6fd22539a27155b24e65fbec03ad0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80953617d98050e74e2487fcf101365b",
+    "url": "/teslacats7776/clickygame/index.html"
+  },
+  {
+    "revision": "451938fd5f97a5da3f93",
+    "url": "/teslacats7776/clickygame/static/css/main.d6dc36f8.chunk.css"
+  },
+  {
+    "revision": "a8afc08aa5ac6c1bdd1d",
+    "url": "/teslacats7776/clickygame/static/js/2.2c33e8b8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/teslacats7776/clickygame/static/js/2.2c33e8b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "451938fd5f97a5da3f93",
+    "url": "/teslacats7776/clickygame/static/js/main.a892a47e.chunk.js"
+  },
+  {
+    "revision": "f9aa074acb8b6d73671b",
+    "url": "/teslacats7776/clickygame/static/js/runtime-main.fc626dd7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/teslacats7776/clickygame/static/media/logo.5d5d9eef.svg"
+  }
+]);
